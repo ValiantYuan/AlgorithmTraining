cb085e6f11e04550058ae7feaeb5f93788e15082
valiant <valiantbest@qq.com>
2019-03-12 01:29:19 +0800

    Tree

---
+++ src/main/java/leetcode/primarytest/linkedlist/DeleteNode.java
@@ -0,0 +1,8 @@
+package leetcode.primarytest.linkedlist;
+
+public class DeleteNode {
+	public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
---
+++ src/main/java/leetcode/primarytest/linkedlist/ListNode.java
@@ -0,0 +1,10 @@
+package leetcode.primarytest.linkedlist;
+
+public 	class ListNode {
+	int val;
+	ListNode next;
+	public ListNode(int x) {
+		// TODO Auto-generated constructor stub
+		this.val = x;
+	}
+}
