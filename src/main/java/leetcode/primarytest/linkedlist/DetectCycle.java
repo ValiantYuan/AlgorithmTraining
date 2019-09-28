988a52f0a8f5dee68fb41b8c4024cf7fc7b2617c
qiyuan.yq <qiyuan.yq@alibaba-inc.com>
2019-09-28 22:01:39 +0800

    循环链表

--- src/main/java/leetcode/primarytest/linkedlist/DetectCycle.java
+++ src/main/java/leetcode/primarytest/linkedlist/DetectCycle.java
@@ -1,5 +1,7 @@
 package leetcode.primarytest.linkedlist;
 
+import basestructure.ListNode;
+
 /**
  * @NAME: DetectCycle
  * @USER: valiant
@@ -7,4 +9,32 @@ package leetcode.primarytest.linkedlist;
  * @DESCRITION:
  **/
 public class DetectCycle {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode intersect = getIntersect(head);
+        if (intersect == null) {
+            return null;
+        }
+        while(head != intersect) {
+            head = head.next;
+            intersect = intersect.next;
+        }
+        return head;
+    }
+
+    public ListNode getIntersect(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        //注意终结条件以fast进行判断
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return slow;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/leetcode/primarytest/linkedlist/HasCycle.java
+++ src/main/java/leetcode/primarytest/linkedlist/HasCycle.java
@@ -10,6 +10,16 @@ import basestructure.ListNode;
  **/
 public class HasCycle {
     public boolean hasCycle(ListNode head) {
-
+        ListNode slow = head;
+        ListNode fast = head;
+        //注意终结条件以fast进行判断
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
     }
 }
